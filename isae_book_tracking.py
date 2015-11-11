ac9e10cf6c05178c932b94e27fb0754eca86df41
rapier83 <rapier83@gmail.com>
2015-11-12 02:40:36 +0900

    get score function complete. DB table changed (Using ISBN)

--- isae_book_tracking.py
+++ isae_book_tracking.py
@@ -1,7 +1,8 @@
 #-*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
+import urllib.request
 import mysql.connector
-from mysql.connector import errorcode
+
 
 # http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9791195623624&orderClick=LEA&Kc=
 # http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9791195623617&orderClick=LAH&Kc=
@@ -14,9 +15,7 @@ from mysql.connector import errorcode
 
 # book_db = {'book_db':[{'title': '고흥, 고흥사람들', 'ISBN' : 9791195623624, 'aladin':69018367, 'yes24':22793592}, {'title': '하트마크', 'ISBN':9791195623617,' aladin' : 69353271, 'yes24' : 22750663}]}
 
-
-
-def get_book_data(book=1, site=1):
+def get_book_data(book, site):
 	config = {
 	'user': 'isaebooks',
 	'password':'isae6200books',
@@ -26,14 +25,7 @@ def get_book_data(book=1, site=1):
 	cnx = mysql.connector.connect(**config)
 	cursor = cnx.cursor()
 
-
-	if site == 1:
-		query_field = "kyobo"
-	if site == 2:
-		query_field = "yes24"
-	if site == 3:
-		query_field = "aladin"
-
+	query_field = "ISBN"	
 	query = ("SELECT id, " + query_field + 
 		" FROM book_list WHERE id='" + str(book) + "'")
 
@@ -42,28 +34,51 @@ def get_book_data(book=1, site=1):
 	for (book_id, query_field) in cursor:
 		target_book = book_id
 		value = query_field
+	
 	cursor.close()
 	cnx.close()
+
 	return (target_book,value)
 
-def get_page_url(book=1, site=1):
+def get_page_url(book, site):
 	kyobo_pre_url = 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode='
 	kyobo_sur_url = '&orderClick=LAH&Kc='
-	alddin_url = 'http://www.aladin.co.kr/shop/wproduct.aspx?ItemId='
-	yes24_pre_url = 'http://www.yes24.com/24/goods/'
-	yes24_sur_url = '?scode=032&OzSrank=1'
+	aladin_pre_url = 'http://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=Book&SearchWord='
+	yes24_pre_url = 'http://www.yes24.com/searchcorner/Search?keywordAd=&keyword=&domain=ALL&qdomain=%C0%FC%C3%BC&Wcode=001_005&query='
 
 	target, value = get_book_data(book,site)
 
 	if site == 1:
 		url = kyobo_pre_url + value + kyobo_sur_url
 	if site == 2:
-		url = yes24_pre_url + value + yes24_sur_url
+		url = yes24_pre_url + value
 	if site == 3:
-		url = aladin_url + value
-	print(url)
+		url = aladin_pre_url + value
+	return (url, site)
 
-	return
+def get_score(url, site):
+	#u = urllib.parse.urlparse(args[0])
+	request = urllib.request.Request(url)
+	res = urllib.request.urlopen(request)
+	html_doc = res.read()
+	soup = BeautifulSoup(html_doc,"html.parser")
+
+	if site == 1:
+		score = (0,site)
+		return score
+	if site == 2:
+		return (0,site)
+	if site == 3:
+		t = soup.find("div",class_="ss_book_list")
+		start_point = t.get_text().index(':') + 2
+		score = int(t.get_text()[start_point:])
+		print(score)
+
+	return (score,site)
 
 
-get_page_url(1,1)
+
+
+
+url, site = get_page_url(1,3)
+get_score(url,site)
--- setup_query.sql
+++ setup_query.sql
@@ -3,14 +3,14 @@ CREATE TABLE `trackers`.`book_list` (
   `id` VARCHAR(45) NOT NULL COMMENT '',
   `title` VARCHAR(100) NOT NULL COMMENT '',
   `yes24` VARCHAR(45) NULL COMMENT '',
-  `kyobo` VARCHAR(45) NULL COMMENT '',
+  `ISBN` VARCHAR(45) NULL COMMENT '',
   `aladin` VARCHAR(45) NULL COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '');
   
   CREATE TABLE `trackers`.`score` (
   `id` INT NOT NULL COMMENT '',
   `time` DATETIME NULL COMMENT '',
-  `kyobo_socre` INT NULL COMMENT '',
+  `kyobo_score` INT NULL COMMENT '',
   `aladin_score` INT NULL COMMENT '',
   `yes24_score` INT NULL COMMENT '',
   `book_id` VARCHAR(100) NULL COMMENT '',
@@ -23,8 +23,10 @@ CREATE TABLE `trackers`.`book_list` (
     ON UPDATE CASCADE);
 
 INSERT INTO `trackers`.`book_list`
-  (`id`, `title`, `yes24`, `kyobo`, `aladin`)
+  (`id`, `title`, `yes24`, `ISBN`, `aladin`)
   VALUES ('1', '하트마크', '22750663', '9791195623617', '69353271');
 INSERT INTO `trackers`.`book_list`
-  (`id`, `title`, `yes24`, `kyobo`, `aladin`)
+  (`id`, `title`, `yes24`, `ISBN`, `aladin`)
   VALUES ('2', '고흥, 고흥 사람들', '22793592', '9791195623624', '69353271');
+  
+SELECT id, ISBN FROM book_list WHERE id='1';
