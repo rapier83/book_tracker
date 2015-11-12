ced14acd1fea2fdd833aa458d2a0d765adc27bdc
rapier83 <rapier83@gmail.com>
2015-11-12 13:56:11 +0900

    1. Fix URL error. Just use `urllib.request.urlopen` 2. Separate functions more 3. Delete comments

--- isae_book_tracking.py
+++ isae_book_tracking.py
@@ -3,19 +3,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 import mysql.connector
 
-
-# http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9791195623624&orderClick=LEA&Kc=
-# http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9791195623617&orderClick=LAH&Kc=
-
-# http://www.aladin.co.kr/shop/wproduct.aspx?ItemId=69018367
-# http://www.aladin.co.kr/shop/wproduct.aspx?ItemId=69353271
-
-# http://www.yes24.com/24/goods/22750663?scode=032&OzSrank=1
-# http://www.yes24.com/24/goods/22793592?scode=032&OzSrank=1
-
-# book_db = {'book_db':[{'title': '고흥, 고흥사람들', 'ISBN' : 9791195623624, 'aladin':69018367, 'yes24':22793592}, {'title': '하트마크', 'ISBN':9791195623617,' aladin' : 69353271, 'yes24' : 22750663}]}
-
-def get_book_data(book, site):
+def get_book_data(book_id, site):
 	config = {
 	'user': 'isaebooks',
 	'password':'isae6200books',
@@ -27,12 +15,12 @@ def get_book_data(book, site):
 
 	query_field = "ISBN"	
 	query = ("SELECT id, " + query_field + 
-		" FROM book_list WHERE id='" + str(book) + "'")
+		" FROM book_list WHERE id='" + str(book_id) + "'")
 
 	cursor.execute(query)
 
-	for (book_id, query_field) in cursor:
-		target_book = book_id
+	for (db_book_id, query_field) in cursor:
+		target_book = db_book_id
 		value = query_field
 	
 	cursor.close()
@@ -40,28 +28,9 @@ def get_book_data(book, site):
 
 	return (target_book,value)
 
-def get_page_url(book, site):
-	kyobo_pre_url = 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode='
-	kyobo_sur_url = '&orderClick=LAH&Kc='
-	aladin_pre_url = 'http://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=Book&SearchWord='
-	yes24_pre_url = 'http://www.yes24.com/searchcorner/Search?keywordAd=&keyword=&domain=ALL&qdomain=%C0%FC%C3%BC&Wcode=001_005&query='
-
-	target, value = get_book_data(book,site)
-
-	if site == 1:
-		url = kyobo_pre_url + value + kyobo_sur_url
-	if site == 2:
-		url = yes24_pre_url + value
-	if site == 3:
-		url = aladin_pre_url + value
-	return (url, site)
-
-def get_score(url, site):
-	#u = urllib.parse.urlparse(args[0])
-	request = urllib.request.Request(url)
-	res = urllib.request.urlopen(request)
-	html_doc = res.read()
-	soup = BeautifulSoup(html_doc,"html.parser")
+def get_score(book_id, site):
+	url = get_url(book_id, site)
+	soup = get_soup(url)
 
 	if site == 1:
 		score = (0,site)
@@ -69,16 +38,43 @@ def get_score(url, site):
 	if site == 2:
 		return (0,site)
 	if site == 3:
-		t = soup.find("div",class_="ss_book_list")
-		start_point = t.get_text().index(':') + 2
-		score = int(t.get_text()[start_point:])
-		print(score)
+		score = get_score_aladin(soup)
 
 	return (score,site)
 
+def get_url(book_id, site):
+	kyobo_pre_url = 'http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode='
+	kyobo_sur_url = '&orderClick=LAH&Kc='
+	aladin_pre_url = 'http://www.aladin.co.kr/search/wsearchresult.aspx?SearchTarget=Book&SearchWord='
+	yes24_pre_url = 'http://www.yes24.com/searchcorner/Search?keywordAd=&keyword=&domain=ALL&qdomain=%C0%FC%C3%BC&Wcode=001_005&query='
+
+	target, value = get_book_data(book_id,site)
+
+	if site == 1:
+		url = kyobo_pre_url + value + kyobo_sur_url
+	if site == 2:
+		url = yes24_pre_url + value
+	if site == 3:
+		url = aladin_pre_url + value
+	
+	return url
+
+def get_soup(url):
+	res = urllib.request.urlopen(url)
+	html_doc = res.read()
+	soup = BeautifulSoup(html_doc, "html.parser")
+	return soup
+
+def get_score_aladin(soup):
+	t = soup.find("div",class_="ss_book_list")
+	start_point = t.get_text().index(':') + 2
+	score = int(t.get_text()[start_point:])
+	return score
+
+def get_score_yes24(soup):
+	return
 
 
 
-
-url, site = get_page_url(1,3)
-get_score(url,site)
+s = get_score(1,3)
+print(s)
